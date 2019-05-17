9978e2883ba40c89c615d528f32820cab5ee84b9
Jason  Montes <jasone.montes@gmail.com>
2019-05-17 06:26:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,12 +8,12 @@ attr_accessor :name, :artist, :genre
 
 
 
-# def initialize(song_name, artist, genre)
-#   @name = song_name
-#     @artist = artist
-#     @genre = genre
-#     @@count += 1 
-#     @@genres << genre
-#     @@artists << artist
-# end
+def initialize(song_name, artist, genre)
+  @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1 
+    @@genres << genre
+    @@artists << artist
+end
 
